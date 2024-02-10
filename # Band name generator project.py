3b521f7a346bdaece9800e8294b1884cdcf99f16
Destiny220 <125221135+Destiny220@users.noreply.github.com>
2024-02-10 01:37:31 +0100

    Delete # Band name generator project.py

--- # Band name generator project.py
+++
@@ -1,7 +0,0 @@
-# Band name generator
-print("Welcome to the band name generator ")
-region = input("What city did you grew up in?\n")
-pet_name = input("What is your pet name?\n")
-print(region)
-print(pet_name)
-print("Your band name could be " + region +" "+ pet_name)
